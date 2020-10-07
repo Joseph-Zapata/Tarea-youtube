0fe9c69a808ed75b3a055c19e5b8c476b15337c2
Joseph Zapata <piedrihitajoseph@gmail.com>
2020-10-07 00:56:22 -0500

    Primer commit
    Se crea la web y se hacen los primeros ajustes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1>Hola!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
