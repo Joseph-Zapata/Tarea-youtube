05c9886d62a119680fa3adc0f301148502fb2fe5
Joseph Zapata <piedrihitajoseph@gmail.com>
2020-10-07 00:59:46 -0500

    Pantalla principal
    Se agrega la parte donde van los videos y las listas de reproduccion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Mainbar from "./components/Mainbar";
+import Hero from "./components/Hero";
 import "./styles/main.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <div className="container mx-auto">
         <Mainbar />
+        <Hero />
       </div>
     </>
   );
